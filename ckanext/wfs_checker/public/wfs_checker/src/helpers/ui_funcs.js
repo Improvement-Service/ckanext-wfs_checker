9dfd393746e03add3b968b86782d20f09f941a18
Nathan Atkinson <n00dl3nate@gmail.com>
2021-08-26 14:24:26 +0100

    added ability to search for esri rest service and wfs service

--- ckanext/wfs_checker/public/wfs_checker/src/helpers/ui_funcs.js
+++ ckanext/wfs_checker/public/wfs_checker/src/helpers/ui_funcs.js
@@ -3,13 +3,29 @@ export function selectElement(id, valueToSelect) {
     element.value = valueToSelect;
 }
 
-export function is_wfs_display_none() {
-    $("#service_type_group").css('display', 'none')
+export function service_type_info_box_hidden(){
+    $("#service_type_info_box_1").css('display', 'none')
 }
 
-export function is_wfs_display_block() {
-    $("#service_type_group").css('display', 'block')
+export function service_type_info_box_block(){
+    $("#service_type_info_box_1").css('display', 'block')
 }
+
+export function layer_name_info_box_esri_rest_content(){
+    var icon = '<i class="fa fa-info-circle"></i>'
+    var text = `You can use a valid ESRI rest MapServer, Group Layer or Feature Layer.
+    If you need to supply multiple layers from this ESRI rest service, please load them individually (as separate resources).
+    `
+    $("#layer_name_info_block").html(`${icon} ${text}`)
+}
+
+export function layer_name_info_box_wfs_content(){
+    var icon = '<i class="fa fa-info-circle"></i>'
+    var text = `If you need to supply multiple layers from this WFS, please load them individually (as
+    separate resources).`
+    $("#layer_name_info_block").html(`${icon} ${text}`)
+}
+
 
 export function service_type_display_none() {
     $("#service_type_group").css('display', 'none')
@@ -34,12 +50,12 @@ export function layer_dropdown_enable(){
     $("#layer_name_dropdown").css('display', 'inline-block'); //make drop down visible
 }
 
-export function wfs_selector_enable(){
-    $("#wfs_selector").css('display', 'inline-block'); //make drop down visible
+export function layer_selector_enable(){
+    $("#layer_selector").css('display', 'inline-block'); //make drop down visible
 }
 
-export function wfs_selector_disable(){
-    $("#wfs_selector").css('display', 'none'); //make drop down visible
+export function layer_selector_disable(){
+    $("#layer_selector").css('display', 'none'); //make drop down visible
 }
 
 export function CursorLoading() {
@@ -50,18 +66,66 @@ export function CursorAuto() {
   document.body.style.cursor = 'auto';
 }
 
-export function show_service_type_dropdown_if_not_none(){
-    var s_type = document.getElementById('service_type').value
-    if (s_type != 'null'){
-        service_type_display_block()
+export function handle_loading() {
+    $("#layer_name_dropdown option").remove() //delete old options hides layer options
+    $('#layer_name_dropdown').append($('<option>', {
+        value: 'null',
+        text: 'Loading...'
+    })); //Add a loading on dropdown while layers are loading.
+}
+
+export function handle_request_fail() {
+    $("#layer_name_dropdown option").remove() //delete old optionshides layer options
+    $('#layer_name_dropdown').append($('<option>', {
+        value: 'null',
+        text: 'No Layers Available, Please Try Again.'
+    })); 
+}
+
+export function reset_layer_dropdown() {
+    $("#layer_name_dropdown option").remove() //delete old optionshides layer options
+    $('#layer_name_dropdown').append($('<option>', {
+        value: 'null',
+        text: 'Click "Get Layers" Button To Populate Dropdown.'
+    })); 
+}
+
+export function add_to_drop_down(options){
+    $('#layer_name_dropdown').empty()
+    if (options.length < 1) {
+        handle_request_fail()
+        return null;
+    } else if (options.length > 1 ){
+        var select = "<option value=" + '' + ">Please Select Layer</option>";
+        $(select).appendTo('#layer_name_dropdown');
+    }
+    $.each(options, function(i, data) {
+        var div_data = "<option value=" + data.value + ">" + data.text + "</option>";
+        $(div_data).appendTo('#layer_name_dropdown');
+    });
+}
+
+export function removeDivIfExists(id) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).remove()
     }
 }
 
-export function show_wfs_dropdown_if_service_type_is_wfs(){
-    var s_type = document.getElementById('service_type').value
-    if (s_type == 'wfs'){
-        layer_dropdown_enable()
-        wfs_selector_enable()
+export function createErrorNotification(errors)  {
+    removeDivIfExists('attribute-errors-div')
+    let div = document.createElement("div");
+    div.id = 'attribute-errors-div'
+    div.className = 'error-explanation alert alert-error'
+    var p = document.createElement('p')
+    p.innerHTML=('Missing Attributes:')
+    var ul = document.createElement("ul")
+    for (let i = 0; i < errors.length; i++) {
+        let li = document.createElement("li")
+        li.innerHTML = `${errors[i]} is missing.`
+        ul.append(li)
     }
+    p.append(ul)
+    div.append(p)
+    let form = document.getElementById('resource-edit')
+    form.prepend(div)
 }
-
--- ckanext/wfs_checker/public/wfs_checker/src/helpers/wfs_getter.js
+++ ckanext/wfs_checker/public/wfs_checker/src/helpers/wfs_getter.js
@@ -1,6 +1,35 @@
+var main = require('../main.js')
+var ui_funcs = require('./ui_funcs.js')
+
 export class WFS_getter {
     constructor(url){
         this.url = url
+        this.webserviceChecker = main.default
+    }
+    
+    cleanUrl(url) {
+        url = decodeURIComponent(String(url));
+        var token = null
+        tokens = ['authkey', 'id', 'key', 'token','authentication']
+        for (var i in tokens) {
+            if (test_on(url, tokens[i]) && token == null){
+                token = match_on(url, tokens[i])[0]
+            }
+        }
+        if(url.includes('?')){
+            url = url.split('?')[0]
+        }
+        url = url + '?request=getcapabilities&service=wfs'
+        if(token != null){
+            url = url + '&' + token;
+        }
+        return url
+    }
+
+    getCurrentURLPath() {
+        let current = window.location.href
+        let base = current.split('/').splice(0,3).join('/')
+        return base
     }
 
     handleResponse(response) {
@@ -11,41 +40,84 @@ export class WFS_getter {
                         status: response.status,
                         statusText: response.statusText,
                     });
-
                     return Promise.reject(error);
                 }
                 return json;
             });
     }
 
+    create_options(data){
+        var options = [];
+        for (var i = 0; i < data.length; i++) {
+            let entry = new Object()
+            entry.text = data[i]['name']
+            entry.value = data[i]['name']
+            options.push(entry)
+        }
+        return options
+    }
+
     get_layers(){
-        var url = `${this.tms_api_url}${this.create_token_endpoint}${this.tms_api_key}`
-        var result = fetch(url)
+        ui_funcs.handle_loading()
+        let url = $("#field-image-url").val().trim()
+        let fetch_url = `${this.getCurrentURLPath()}/api/3/action/get_wfs_layers`
+        var payload = {
+            url: this.cleanUrl(url)
+        };
+        var data = new FormData();
+        data.append( "json", JSON.stringify( payload ) );
+        fetch(fetch_url,{
+            method: "POST",
+            body: data
+        })
         .then(this.handleResponse)
         .then((resp) =>{
             if (resp.status_code = 200){
-                this.tms_token = resp.token
-                this.getGeneralNotices()
-                this.getLicensingNotices()
-                this.getPlanningNotices()
-                this.getTrafficNotices()
+                console.log(resp)
+                if (resp.result.error){
+                    console.error(resp.result.error)
+                    ui_funcs.handle_request_fail()
+                    throw resp.result.error
+                } else{
+                    var options = this.create_options(resp.result)
+                    ui_funcs.add_to_drop_down(options)
+                }
             } else {
-                console.error('Failed to get TMS token')
+                alert("Unable to retrieve layers from WFS please continue");
+                console.error('Request Failed')
+                ui_funcs.handle_request_fail()
             }
         })
         .catch(error => {
             console.warn(error);
+            ui_funcs.handle_request_fail()
+        }).finally(()=> {
+            ui_funcs.CursorAuto()
         });
-        return result
+    }
+
+    matchRuleShort(str, rule) {
+        var escapeRegex = (str) => str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+        return new RegExp("^" + rule.split("*").map(escapeRegex).join(".*") + "$").test(str);
+    }
+    
+    match_on(str, key) {
+        var re = RegExp(`(${key}=([a-z-A-Z-0-9_"£$%*^~])+)`)
+        return re.exec(str);
+    }
+    
+    test_on(str, key) {
+        var re = RegExp(`(${key}=([a-z-A-Z-0-9_"£$%*^~])+)`)
+        return re.test(str);
     }
 
     cleanUrl(url) {
         url = decodeURIComponent(String(url));
         var token = null
-        tokens = ['authkey', 'id', 'key', 'token','authentication']
+        var tokens = ['authkey', 'id', 'key', 'token','authentication']
         for (var i in tokens) {
-            if (test_on(url, tokens[i]) && token == null){
-                token = match_on(url, tokens[i])[0]
+            if (this.test_on(url, tokens[i]) && token == null){
+                token = this.match_on(url, tokens[i])[0]
             }
         }
         if(url.includes('?')){

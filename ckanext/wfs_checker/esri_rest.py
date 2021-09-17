579923a5d0468d4e58032190c3c3d000f9565329
Nathan Atkinson <n00dl3nate@gmail.com>
2021-09-17 14:15:24 +0100

    code clean up

--- ckanext/wfs_checker/esri_rest.py
+++ ckanext/wfs_checker/esri_rest.py
@@ -9,67 +9,100 @@ class ESRI_REST():
         self.base_url = self._get_base_url()
         self.params = self._get_params()
         self.routes = self._get_route()
-        
+
     def _get_base_url(self):
         return f"{self.url.split('rest/services')[0]}rest/services"
-        
+
     def _get_params(self):
         if '?' in self.url and self.url.split('?')[1].strip != "":
             params = self.url.split('?')[1].split("&")
             return [{'key':p.split('=')[0],'value':p.split('=')[1]} for p in params]
-        else:
-            return []
-    
+        return []
+
     def _get_route(self):
-        x = self.url.split(self._get_base_url())
-        if '?' in x[1]:
-            x = x[1].split('?')[0]
-        else: 
-            x = x[1]
-        routes = list(filter(None, x.split('/')))
+        item = self.url.split(self._get_base_url())
+        if '?' in item[1]:
+            item = item[1].split('?')[0]
+        else:
+            item = item[1]
+        routes = list(filter(None, item.split('/')))
         for route in routes:
             if route.lower() in ['layers', 'legends']:
                 routes.remove(route)
         return routes
 
-    def _get_data(self, url):
-        x = requests.get(url)
-        y = json.loads(x.text)
-        if 'error' in y.keys():
-            return {'error':y['error'], 'status':'fail'}
-        else:
-            return y
-    
+    @staticmethod
+    def _get_data(url):
+        data = requests.get(url)
+        data_json = json.loads(data.text)
+        if 'error' in data_json.keys():
+            return {'error':data_json['error'], 'status':'fail'}
+        return data_json
+
     def add_json_format_to_params(self):
+        """
+        removes any existing format parameter and add in json format
+        """
         res = list(filter(lambda i: i['key'] != 'f', self.params))
-        res.append({'key':'f','value':'json'})
+        res.append({'key':'f', 'value':'json'})
         self.params = res
-    
-    def create_params_string(self, params):
-        s = ""
-        for i in range(0,len(params)):
+
+    @staticmethod
+    def create_params_string(params):
+        """creates a formatted params string form list of strings
+
+        Args:
+            params ([string]): [list of strings]
+
+        Returns:
+            Stings: formatted string
+        """
+        string = ""
+        for i in range(0, len(params)):
             if i == 0:
-                s = s + f"{params[i]['key']}={params[i]['value']}"
+                string = string + f"{params[i]['key']}={params[i]['value']}"
             else:
-                s = s + f"&{params[i]['key']}={params[i]['value']}"
-        return s
-    
-    def is_feature_layer(self, url):
-        x = requests.get(url)
-        y = json.loads(x.text)
-        return y.get('type', 'unknown')
-    
+                string = string + f"&{params[i]['key']}={params[i]['value']}"
+        return string
+
     def create_url(self, route, params):
-        r = '/'.join(route)
-        p = self.create_params_string(params)
-        return f"{self.base_url}/{r}?{p}"
-    
+        """creates a esri rest url
+
+        Args:
+            route ([sting]): list of strings
+            params ([string]): list of strings
+
+        Returns:
+            string: url string
+        """
+        routes = '/'.join(route)
+        parametes = self.create_params_string(params)
+        return f"{self.base_url}/{routes}?{parametes}"
+
     def create_layer_url(self, route, params):
-        r = '/'.join(route)
-        p = self.create_params_string(params)
-        return f"{self.base_url}/{r}/layers?{p}"
+        """create esri rest url for layer list
+
+        Args:
+            route ([sting]): list of strings
+            params ([string]): list of strings
+
+        Returns:
+            string: url string
+        """
+        routes = '/'.join(route)
+        parametes = self.create_params_string(params)
+        return f"{self.base_url}/{routes}/layers?{parametes}"
     
-    def get_esri_route_type(self, data_dict):
+    @staticmethod
+    def get_esri_route_type(data_dict):
+        """get type from esri rest url
+
+        Args:
+            data_dict (Dictionary): Esri rest data
+
+        Returns:
+            String: type of rest url e.g. Feature Layer, Group Layer, Folder
+        """
         route_type = data_dict.get('type', None) 
         if 'error' in data_dict.keys():
             return 'Error'
@@ -83,8 +116,17 @@ class ESRI_REST():
             return 'Folder'
         else:
             return 'Unknown'
-    
-    def get_layers_from_services(self, routes, params, data_dict):
+
+    def get_layers_from_services(self, params, data_dict):
+        """get all layers for a esri rest services
+
+        Args:
+            params ([string]): list of strings
+            data_dict (Dictionary): esri rest data
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         layers = []
         for service in data_dict['services']:
             service_routes = [service['name'], service['type']]
@@ -92,10 +134,19 @@ class ESRI_REST():
             layers = layers + s_layers
         return layers
     
-    def get_layers_from_folder(self, routes, params, data_dict):
+    def get_layers_from_folder(self, params, data_dict):
+        """get all layers for a esri rest folder
+
+        Args:
+            params ([string]): list of strings
+            data_dict (Dictionary): esri rest data
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         layers = []
         if data_dict['services']:
-            s_layers = self.get_layers_from_services(routes, params, data_dict)
+            s_layers = self.get_layers_from_services(params, data_dict)
             layers = layers + s_layers
         if data_dict['folders']:
             for folder_name in data_dict['folders']:
@@ -104,19 +155,28 @@ class ESRI_REST():
                 if 'error' in f_data_dict.keys():
                     pass
                 else:
-                    s_layers = self.get_layers_from_folder([folder_name], params, f_data_dict)
+                    s_layers = self.get_layers_from_folder(params, f_data_dict)
                     layers = layers + s_layers
         return layers
-        
+
     def get_layers_from_service(self, routes, params):
+        """get all layers for a esri rest service
+
+        Args:
+            routes ([string]): list of strings
+            params ([string]): list of strings
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         url = self.create_layer_url(routes, params)
-        r = requests.get(url)
-        data_dict = json.loads(r.text)
+        data = requests.get(url)
+        data_dict = json.loads(data.text)
         layers = data_dict.get('layers', [])
         result = []
         for layer in layers:
             if layer['type'] == 'Feature Layer':
-                url = self.create_url([*routes,str(layer['id'])], [])
+                url = self.create_url([*routes, str(layer['id'])], [])
                 obj = {
                     'name' : layer['name'],
                     'id': layer['id'],
@@ -126,13 +186,23 @@ class ESRI_REST():
         return result
 
     def get_layers_from_group_layer(self, routes, params, data_dict):
+        """get all layers from esri group layer.
+
+        Args:
+            routes ([string]): list of strings
+            params ([string]): list of strings
+            data_dict (Dictionary): esri rest data
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         result = []
         service_url = self.create_layer_url(routes[:-1], params)
         data = self._get_data(service_url)
-        sub_layer_ids = [ x['id'] for x in data_dict['subLayers']]
+        sub_layer_ids = [x['id'] for x in data_dict['subLayers']]
         for layer in data['layers']:
             if layer['id'] in sub_layer_ids and layer['type'] == 'Feature Layer':
-                url = self.create_url([*routes[:-1],str(layer['id'])], [])
+                url = self.create_url([*routes[:-1], str(layer['id'])], [])
                 obj = {
                     'name' : layer['name'],
                     'id': layer['id'],
@@ -140,29 +210,46 @@ class ESRI_REST():
                 }
                 result.append(obj)
         return result
-    
-    def get_layer_from_feature_layer(self, routes, params, data_dict):
+
+    def get_layer_from_feature_layer(self, routes, data_dict):
+        """gets layer data from esri rest data
+
+        Args:
+            routes ([string]): list of strings
+            data_dict (Dictionary): esri rest data
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         url = self.create_url(routes, [])
-        layer = {
-                'name' : data_dict['name'],
-                'id': data_dict['id'],
-                'url':url
-                }
+        layer = {'name' : data_dict['name'], 'id': data_dict['id'], 'url':url}
         return [layer]
-        
+
     def get_layers(self, routes=None, params=None):
+        """get esri rest layers
+
+        Args:
+            routes ([string], optional): [description]. Defaults to None.
+            params ([string], optional): [description]. Defaults to None.
+
+        Raises:
+            ValueError: if type of feature layer is 'Folder' or 'unknown'
+
+        Returns:
+            [Dictionary]: list of dictionarys
+        """
         self.add_json_format_to_params()
         routes = self.routes if routes is None else routes
         params = self.params if params is None else params
         url = self.create_url(routes, params)
-        x = requests.get(url)
-        data_dict = json.loads(x.text)
+        data = requests.get(url)
+        data_dict = json.loads(data.text)
         route_type = self.get_esri_route_type(data_dict)
         layers = None
         if route_type == 'Service':
             layers = self.get_layers_from_service(routes, params)
         elif route_type == 'Feature Layer':
-            layers = self.get_layer_from_feature_layer(routes, params, data_dict)
+            layers = self.get_layer_from_feature_layer(routes, data_dict)
         elif route_type == 'Group Layer':
             layers = self.get_layers_from_group_layer(routes, params, data_dict)
         elif route_type == 'Folder':
@@ -170,5 +257,3 @@ class ESRI_REST():
         else:
             raise ValueError('ESRI Rest Service URL Must be a Service, Group Layer or Feature Layer')
         return layers
-        
-        
--- ckanext/wfs_checker/plugin.py
+++ ckanext/wfs_checker/plugin.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-member, unused-argument
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import json 
@@ -5,16 +6,26 @@ import logging
 from owslib.wfs import WebFeatureService
 from .esri_rest import ESRI_REST
 
-log = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 @toolkit.side_effect_free
 def get_wfs_layers(context, data_dict=None):
+    """get all wfs layers in a wfs service
+
+    Args:
+        context ([type]): [description]
+        data_dict (Dictionary, optional): contains url key value pair. Defaults to None.
+
+    Returns:
+        [dictionary]: list of dictionarys
+    """
     data = json.loads(data_dict['json'])
     if 'url' in data.keys():
         url = str(data['url'])
         try:
-            wfs = WebFeatureService(url=url,  version='1.1.0')
-        except Exception as e:
+            wfs = WebFeatureService(url=url, version='1.1.0')
+        except RuntimeError as error:
+            LOGGER.error(error)
             return {'error':'URL provided is not a valid wfs'}
     else:
         return {'error':"No url field provided. Please specify an url."}
@@ -25,11 +36,19 @@ def get_wfs_layers(context, data_dict=None):
         results.append({
             'name': layer
         })
-    
     return results
 
 @toolkit.side_effect_free
 def get_esri_rest_layers(context, data_dict=None):
+    """get all layer from esri rest service
+
+    Args:
+        context ([type]): [description]
+        data_dict (Dictionary, optional): contains url key value pair. Defaults to None.
+
+    Returns:
+        [dictionary]: list of dictionarys
+    """
     data = json.loads(data_dict['json'])
     if 'url' in data.keys():
         url = str(data['url'])
@@ -37,24 +56,36 @@ def get_esri_rest_layers(context, data_dict=None):
             esri_rest = ESRI_REST(url)
             resp = esri_rest.get_layers()
             return resp
-        except Exception as e:
-            log.error(e)
-            return {'error': 'Url is not a valid ESRI rest service' }
+        except ValueError as error:
+            LOGGER.error(error)
+            return {'error': str(error)}
+        except RuntimeError as error:
+            LOGGER.error(error)
+            return {'error': 'Url is not a valid ESRI rest service'}
     else:
         return {'error':"No url field provided. Please specify an url."}
 
 def get_esri_rest_helper(url, item_id):
+    """get a specific esri rest layer by layer id, this is
+    used in update page to get the name of the previously saved layer
+    as only the layer_id is stored in metadata.
+    Args:
+        url (sting): url of esri rest service
+        item_id (string or int): id of layer
+
+    Returns:
+        string: name of layer if request is valid
+        string: returns orginal layer id
+    """
     try:
         esri_rest = ESRI_REST(url)
-        routes = esri_rest.routes
-        params = esri_rest.params
         resp = esri_rest.get_layers()
-        for x in resp:
-            if str(x['id']) == str(item_id):
-                return x['name']
+        for layer in resp:
+            if str(layer['id']) == str(item_id):
+                return layer['name']
         return item_id
-    except Exception as e:
-        log.error(e)
+    except ValueError as error:
+        LOGGER.error(error)
         return item_id
 
 class Wfs_CheckerPlugin(plugins.SingletonPlugin):
@@ -62,16 +93,27 @@ class Wfs_CheckerPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.interfaces.IActions)
     plugins.implements(plugins.ITemplateHelpers)
 
-    def get_helpers(self):
+    @staticmethod
+    def get_helpers():
+        """ Return a dict mapping names to helper functions.
+        """
         return {'get_esri_rest_helper': get_esri_rest_helper}
-    
+
     #IConfigurer
-    def update_config(self, config_):
+    @staticmethod
+    def update_config(config_):
+        """ Returns a string representing the location of the 
+            template to be rendered when the view is displayed
+        """
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'wfs_checker')
 
-    def get_actions(self):
+    @staticmethod
+    def get_actions():
+        """ Should return a dict, the keys being the name of 
+            the logic function and the values being the functions themselves.
+        """
         # Registers the custom API method defined above
         return {'get_wfs_layers': get_wfs_layers,
                 'get_esri_rest_layers': get_esri_rest_layers}
